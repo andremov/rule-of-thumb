e05000cd717d8a45b3c05edaaa160a7d0cc6adf6
Andrés Movilla <842843+andremov@users.noreply.github.com>
2022-02-15 21:32:37 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+    render(): JSX.Element {
+        return <div className="app">Edit src/App.js and save to reload.</div>
+    }
+}
