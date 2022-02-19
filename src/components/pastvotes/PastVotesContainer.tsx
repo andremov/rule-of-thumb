ea4ebcd7d5b11c8dfc3f4db89937100404d94d26
Andres-XL <amovilla1103@gmail.com>
2022-02-19 17:27:16 -0500

    Add: Grid view

--- src/components/pastvotes/PastVotesContainer.tsx
+++ src/components/pastvotes/PastVotesContainer.tsx
@@ -6,22 +6,37 @@ import { getPolls } from '../../utils/pollSlice'
 import { PollInterface } from '../../utils/types'
 
 export function PastVotesContainer(): JSX.Element {
+    const [view, setView] = React.useState('List')
     const data: PollInterface[] = useSelector(getPolls)
+
+    const handleViewChange = (e: React.ChangeEvent<HTMLSelectElement>): void => setView(e.target.value)
 
     if (!data) {
         return (
             <main role="main">
+                <h2>Previous Rulings</h2>
                 <div className={'spinner'} />
+                <div className={'spinner-text'}>Loading content...</div>
             </main>
         )
     }
 
     return (
         <main role="main">
-            <h2>Previous Rulings</h2>
-            {data.map((item: PollInterface) => (
-                <PastVotesCard key={item._id} {...item} />
-            ))}
+            <div className={'post-votes-container__title'}>
+                <h2>Previous Rulings</h2>
+                <select onChange={handleViewChange}>
+                    <option>List</option>
+                    <option>Grid</option>
+                </select>
+            </div>
+            <div className={`post-votes-container ${view.toLocaleLowerCase()}-view`}>
+                <div className={'post-votes-container__scroll'}>
+                    {data.map((item: PollInterface) => (
+                        <PastVotesCard key={item._id} {...item} />
+                    ))}
+                </div>
+            </div>
         </main>
     )
 }
